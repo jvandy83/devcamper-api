124284e8be720e834b7fe0b70b4fcac58cfdca30
Jared Vandeventer <vanthedev@gmail.com>
2019-12-18 15:58:08 -0700

    added middleware to prevent xss and perform sanitization

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,25 +1,44 @@
 const express = require('express');
-const router = express.Router();
 const {
   getBootcamps,
   getBootcamp,
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
-  getBootcampsInRadius
+  getBootcampsInRadius,
+  bootcampPhotoUpload
 } = require('../controllers/bootcamps');
+
+const Bootcamp = require('../models/Bootcamp');
+
+// Include other resourse routers
+const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
+
+const router = express.Router();
+
+const { protect, authorize } = require('../middleware/auth');
+const advancedResults = require('../middleware/advancedResults');
+
+// Re-route into other resourse routers
+router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
+
+router
   .route('/')
-  .get(getBootcamps)
-  .post(createBootcamp);
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 module.exports = router;
